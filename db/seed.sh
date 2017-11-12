e001759eb4ed57b7ad2771f71d9232ba658c1415
pedrojosepajares <pedrojosepajares@gmail.com>
2017-11-12 21:30:56 +0100

    Carga de valores iniciales en la base de datos

---
+++ db/seed.sh
@@ -0,0 +1,3 @@
+mongoimport --db meeting-tracker --collection login --file ./db/login.json --jsonArray
+mongoimport --db meeting-tracker --collection positions --file ./db/positions.json --jsonArray
+mongoimport --db meeting-tracker --collection meetings --file ./db/meetings.json --jsonArray
